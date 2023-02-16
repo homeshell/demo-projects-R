9f0edefd2c89462df6eac99257a5d7c4d87f92df
Kyle <122122891+homeshell@users.noreply.github.com>
2023-02-16 16:48:18 -0500

    Add files via upload
    First time playing around with R. Lots more to come!

---
+++ R_Code.R
@@ -0,0 +1,14 @@
+# Create three variables
+x <- 1
+y <- 2
+z <- 3
+# 
+# Sum the three variables
+total <- x + y + z
+print(total)  # Display Result
+#
+# Calculate BMI
+height <- 1.82  # Meters
+weight <- 96.5  # Kilograms
+bmi <- weight/(height*height)  # Calculation
+print(bmi)  # Display result
